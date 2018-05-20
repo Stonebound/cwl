d1c9892b83cfb918fa80b1837b24f3248b57ee84
phit <phit@hush.com>
2018-05-20 09:01:52 +0200

    added closed mode and added bypass permission

--- src/main/java/ru/pearx/cwl/CWL.java
+++ src/main/java/ru/pearx/cwl/CWL.java
@@ -7,14 +7,16 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
 import com.google.inject.Inject;
-import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
 import org.slf4j.Logger;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.config.DefaultConfig;
+import org.spongepowered.api.entity.living.player.User;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.Order;
+import org.spongepowered.api.event.filter.Getter;
 import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
@@ -22,6 +24,7 @@ import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.profile.GameProfile;
 import org.spongepowered.api.scheduler.Task;
 import org.spongepowered.api.service.sql.SqlService;
+import org.spongepowered.api.service.user.UserStorageService;
 import org.spongepowered.api.service.whitelist.WhitelistService;
 import org.spongepowered.api.text.serializer.TextSerializers;
 import ru.pearx.cwl.commands.GenWhitelistCommand;
@@ -36,6 +39,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -43,12 +48,8 @@ import java.util.concurrent.TimeUnit;
 import javax.annotation.Nullable;
 import javax.sql.DataSource;
 
-
-/**
- * Created by me on 23.02.17.
- */
 @Plugin(id = "cwl", name = "Custom Whitelist", authors = "mrAppleXZ", description = "Customize your whitelist message & generate the whitelist!",
-        version = "1.3.0")
+        version = "1.3.1")
 public class CWL {
     public static CWL INSTANCE;
     private CommentedConfigurationNode config = null;
@@ -108,7 +109,8 @@ public class CWL {
                 config = configManager.load();
                 this.config.getNode("main", "whitelist_msg").setValue("Send a whitelist request firstly!").setComment("Message displayed to not whitelisted users");
                 this.config.getNode("main", "sync_delay").setValue(120);
-                this.config.getNode("main", "mode").setValue("none").setComment("Modes: db, json, none");
+                this.config.getNode("main", "mode").setValue("none").setComment("Modes: db, json, none, closed");
+                this.config.getNode("main", "bypass").setValue("").setComment("Comma separated list of users that can join when closed.");
 
                 //dbConnection = Sponge.getServiceManager().provide(SqlService.class).get().getDataSource(this.config.getNode("db", "db_connection").getString());
                 this.config.getNode("db", "db_connection").setValue("");
@@ -122,7 +124,6 @@ public class CWL {
         } catch (IOException ex) {
             getLog().error("Couldn't create default configuration file!");
         }
-
     }
 
     public void syncWhitelist() throws SQLException {
@@ -132,7 +133,7 @@ public class CWL {
             }
             getLog().info("Syncing whitelist...");
             WhitelistService wh = Sponge.getServiceManager().provide(WhitelistService.class).get();
-            wh.getWhitelistedProfiles().clear();
+            wh.getWhitelistedProfiles().stream().forEach(gameProfile -> wh.removeProfile(gameProfile));
             try (Connection conn = getDbConnection().getConnection()) {
                 try (PreparedStatement st = conn.prepareStatement(getDbQuery())) {
                     ResultSet res = st.executeQuery();
@@ -146,6 +147,29 @@ public class CWL {
                 }
             }
             getLog().info("The whitelist was successfully synced!");
+        } else if (getMode().equals("closed")) {
+            WhitelistService wh = Sponge.getServiceManager().provide(WhitelistService.class).get();
+            wh.getWhitelistedProfiles().stream().forEach(gameProfile -> wh.removeProfile(gameProfile));
+            // wh.addProfile(GameProfile.of(UUID.fromString("069a79f4-44e9-4726-a5be-fca90e38aaf5"), "notch"));
+            // getLog().error("whitelist:" + Arrays.toString(wh.getWhitelistedProfiles().toArray()));
+            List<String> bypassList = Arrays.asList(this.config.getNode("main", "bypass").getString().split(","));
+            if (!bypassList.isEmpty()) {
+                for (String bypasser : bypassList) {
+                    Optional<UserStorageService> optStorage = Sponge.getServiceManager().provide(UserStorageService.class);
+                    if (optStorage.isPresent() && !bypasser.equals("")){
+                        Optional<User> usr = optStorage.get().get(UUID.fromString(bypasser));
+                        if (usr.isPresent()) {
+                            wh.addProfile(GameProfile.of(UUID.fromString(bypasser), usr.get().getName()));
+                        } else {
+                            wh.addProfile(GameProfile.of(UUID.fromString(bypasser), "bypasser"));
+                        }
+                    }
+                }
+            }
+            if (syncTask != null) {
+                syncTask.cancel();
+            }
+            getLog().info("Whitelist closed!");
         } else if (getMode().equals("json")) {
             try {
                 URL url = new URL(this.config.getNode("json", "json_url").getString());
@@ -155,7 +179,7 @@ public class CWL {
                     JsonParser parser = new JsonParser();
                     JsonArray userArray = parser.parse(reader).getAsJsonArray();
                     WhitelistService wh = Sponge.getServiceManager().provide(WhitelistService.class).get();
-                    wh.getWhitelistedProfiles().clear();
+                    wh.getWhitelistedProfiles().stream().forEach(gameProfile -> wh.removeProfile(gameProfile));
                     for (JsonElement user : userArray) {
                         JsonObject userObj = user.getAsJsonObject();
                         String name = userObj.get("name").getAsString();
@@ -181,9 +205,7 @@ public class CWL {
             syncTask.cancel();
         }
         if (getSecondsSyncDelay() >= 1) {
-            syncTask = Task.builder().execute(
-                    () ->
-                    {
+            syncTask = Task.builder().execute(() -> {
                         try {
                             syncWhitelist();
                         } catch (Exception e1) {
@@ -208,12 +230,12 @@ public class CWL {
     public void omServerStarted(GameStartedServerEvent e) throws IOException {
         INSTANCE = this;
         Sponge.getCommandManager().register(this,
-                CommandSpec.builder().permission("cwl.command").child(
-                        CommandSpec.builder().permission("cwl.command.whitelist").executor(new GenWhitelistCommand(this)).build(), "whitelist",
-                        "gen-whitelist"
+                CommandSpec.builder().permission("cwl.command.base").child(
+                        CommandSpec.builder().permission("cwl.command.whitelist")
+                                .executor(new GenWhitelistCommand(this)).build(), "whitelist", "gen-whitelist"
                 ).child(
-                        CommandSpec.builder().permission("cwl.command.reload").executor(new ReloadConfigCommand(this)).build(), "reload",
-                        "reload-config"
+                        CommandSpec.builder().permission("cwl.command.reload")
+                                .executor(new ReloadConfigCommand(this)).build(), "reload", "reload-config"
                 ).build(), "cwl");
         try {
             reload();
@@ -222,14 +244,14 @@ public class CWL {
         }
     }
 
-    @Listener
-    public void onClientConnection(ClientConnectionEvent.Auth e) {
+    @Listener(order = Order.FIRST)
+    public void onClientConnection(ClientConnectionEvent.Auth event) {
         if (Sponge.getServer().hasWhitelist()) {
             Optional<WhitelistService> wls = Sponge.getServiceManager().provide(WhitelistService.class);
             if (wls.isPresent()) {
-                if (!wls.get().isWhitelisted(e.getProfile())) {
-                    e.setCancelled(true);
-                    e.setMessage(TextSerializers.FORMATTING_CODE.deserialize(getWhitelistMessage()));
+                if (!wls.get().isWhitelisted(event.getProfile())) {
+                    event.setCancelled(true);
+                    event.setMessage(TextSerializers.FORMATTING_CODE.deserialize(getWhitelistMessage()));
                 }
             }
         }
